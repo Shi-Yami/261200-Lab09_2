5d18d3d4c9b25070504c3be96dbb29cf9c77f071
Shi-Yami <aungthiha_1@cmu.ac.th>
2025-09-03 21:22:11 +0700

    Create Eagle.java

---
+++ Eagle.java
@@ -0,0 +1,13 @@
+package oop.lsp;
+
+public class Eagle implements Flyable {
+    @Override
+    public void move() {
+        System.out.println("Eagle is walking");
+    }
+
+    @Override
+    public void fly() {
+        System.out.println("Eagle is soaring");
+    }
+}
