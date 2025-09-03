5e9d333444ac9e6c1ec381909bdcadb240cee63f
Shi-Yami <aungthiha_1@cmu.ac.th>
2025-09-03 21:15:35 +0700

    Create Sparrow.java

---
+++ Sparrow.java
@@ -0,0 +1,13 @@
+package oop.lsp;
+
+public class Sparrow implements Flyable {
+    @Override
+    public void move() {
+        System.out.println("Sparrow is hopping");
+    }
+
+    @Override
+    public void fly() {
+        System.out.println("Sparrow is flying");
+    }
+}
